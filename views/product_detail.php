1c76293d9b3c75bf6285eab3a7af65f361124ed4
muharyman <muharymank@gmail.com>
2018-12-26 15:02:26 +0700

    finish

--- views/product_detail.php
+++ views/product_detail.php
@@ -98,12 +98,12 @@
 					</div>
 						';
 					?>
-					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<a class="carousel-control-prev geser" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon " aria-hidden="true"></span>
 						<span class="sr-only">Previous</span>
 					</a>
-					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<a class="carousel-control-next geser" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon " aria-hidden="true"></span>
 						<span class="sr-only">Next</span>
 					</a>
 				</div>
